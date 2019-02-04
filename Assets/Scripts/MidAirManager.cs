c0734283760b33773f1afd260c1a681c98d7ede3
Oliver Ellmers <oliver@random42.com>
2019-02-04 17:26:02 +0000

    UI Manager now working correctly

---
+++ Assets/Scripts/MidAirManager.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class MidAirManager : MonoBehaviour
+{
+
+    public MidAirPositionerBehaviour m_MidAirPositioner;
+    public GameObject m_MidAirAugmentation;
+    public AnchorBehaviour m_MidAirAnchor;
+    public ContentPositioningBehaviour m_ContentPositioningBehaviour;
+
+    public static bool AnchorExists
+    {
+        get { return anchorExists; }
+        private set { anchorExists = value; }
+    }
+
+    StateManager m_StateManager;
+    SmartTerrain m_SmartTerrain;
+    PositionalDeviceTracker m_PositionalDeviceTracker;
+    
+    int AutomaticHitTestFrameCount;
+    int m_AnchorCounter;
+    static bool anchorExists;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VuforiaARController.Instance.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+        VuforiaARController.Instance.RegisterOnPauseCallback(OnVuforiaPaused);
+        DeviceTrackerARController.Instance.RegisterTrackerStartedCallback(OnTrackerStarted);
+        DeviceTrackerARController.Instance.RegisterDevicePoseStatusChangedCallback(OnDevicePoseStatusChanged);
+
+        //m_MidAirAnchor = m_MidAirAugmentation.GetComponentInParent<AnchorBehaviour>();
+        UtilityHelper.EnableRendererColliderCanvas(m_MidAirAugmentation, false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!VuforiaRuntimeUtilities.IsPlayMode() && !AnchorExists)
+        {
+            AnchorExists = DoAnchorsExist();
+        }
+
+    }
+
+    void OnDestroy()
+    {
+        Debug.Log("OnDestroy() called.");
+
+        VuforiaARController.Instance.UnregisterVuforiaStartedCallback(OnVuforiaStarted);
+        VuforiaARController.Instance.UnregisterOnPauseCallback(OnVuforiaPaused);
+        DeviceTrackerARController.Instance.UnregisterTrackerStartedCallback(OnTrackerStarted);
+        DeviceTrackerARController.Instance.UnregisterDevicePoseStatusChangedCallback(OnDevicePoseStatusChanged);
+    }
+
+    public void PlaceObjectInMidAir(Transform midAirTransform)
+    {
+        Debug.Log("PlaceObjectInMidAir() called.");
+
+        m_ContentPositioningBehaviour.AnchorStage = m_MidAirAnchor;
+        Debug.Log("1. m_ContentPositioningBehaviour.AnchorStage OR m_MidAirArchor is not null -> proceed");
+
+
+        m_ContentPositioningBehaviour.PositionContentAtMidAirAnchor(midAirTransform);
+        Debug.Log("2. m_ContentPositioningBehaviour.PositionContentAtMidAirAnchor(midAirTransform) -> proceed");
+
+        UtilityHelper.EnableRendererColliderCanvas(m_MidAirAugmentation, true);
+        Debug.Log("3. UtilityHelper.EnableRendererColliderCanvas(m_MidAirAugmentation, true); -> proceed");
+
+        m_MidAirAugmentation.transform.localPosition = Vector3.zero;
+        Debug.Log("4. m_MidAirAugmentation.transform.localPosition = Vector3.zero; -> proceed");
+
+        UtilityHelper.RotateTowardCamera(m_MidAirAugmentation);
+        Debug.Log("5. UtilityHelper.RotateTowardCamera(m_MidAirAugmentation); -> out");
+    }
+
+    public void ResetScene()
+    {
+        Debug.Log("ResetScene() called.");
+
+        m_MidAirAugmentation.transform.position = Vector3.zero;
+        m_MidAirAugmentation.transform.localEulerAngles = Vector3.zero;
+        UtilityHelper.EnableRendererColliderCanvas(m_MidAirAugmentation, false);
+
+        DeleteAnchors();
+    }
+
+    public void ResetTrackers()
+    {
+        Debug.Log("ResetTrackers() called.");
+
+        m_SmartTerrain = TrackerManager.Instance.GetTracker<SmartTerrain>();
+        m_PositionalDeviceTracker = TrackerManager.Instance.GetTracker<PositionalDeviceTracker>();
+
+        // Stop and restart trackers
+        m_SmartTerrain.Stop(); // stop SmartTerrain tracker before PositionalDeviceTracker
+        m_PositionalDeviceTracker.Reset();
+        m_SmartTerrain.Start(); // start SmartTerrain tracker after PositionalDeviceTracker
+    }
+
+    void DeleteAnchors()
+    {
+        m_MidAirAnchor.UnConfigureAnchor();
+        AnchorExists = DoAnchorsExist();
+    }
+
+    bool DoAnchorsExist()
+    {
+        if (m_StateManager != null)
+        {
+            IEnumerable<TrackableBehaviour> trackableBehaviours = m_StateManager.GetActiveTrackableBehaviours();
+
+            foreach (TrackableBehaviour behaviour in trackableBehaviours)
+            {
+                if (behaviour is AnchorBehaviour)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    void OnVuforiaStarted() {
+        Debug.Log("OnVuforiaStarted() called.");
+
+        m_StateManager = TrackerManager.Instance.GetStateManager();
+        m_PositionalDeviceTracker = TrackerManager.Instance.GetTracker<PositionalDeviceTracker>();
+        m_SmartTerrain = TrackerManager.Instance.GetTracker<SmartTerrain>();
+        if (m_PositionalDeviceTracker != null && m_SmartTerrain != null)
+        {
+            if (!m_PositionalDeviceTracker.IsActive)
+                m_PositionalDeviceTracker.Start();
+            if (m_PositionalDeviceTracker.IsActive && !m_SmartTerrain.IsActive)
+                m_SmartTerrain.Start();
+        }
+        else
+        {
+            if (m_PositionalDeviceTracker == null)
+                Debug.Log("PositionalDeviceTracker returned null. GroundPlane not supported on this device.");
+            if (m_SmartTerrain == null)
+                Debug.Log("SmartTerrain returned null. GroundPlane not supported on this device.");
+        }
+    }
+
+    void OnVuforiaPaused(bool paused)
+    {
+        Debug.Log("OnVuforiaPaused(" + paused.ToString() + ") called.");
+
+        if (paused) {
+            //ResetScene();
+        }
+    }
+
+    void OnTrackerStarted()
+    {
+        Debug.Log("OnTrackerStarted() called.");
+
+        m_PositionalDeviceTracker = TrackerManager.Instance.GetTracker<PositionalDeviceTracker>();
+        m_SmartTerrain = TrackerManager.Instance.GetTracker<SmartTerrain>();
+
+        if (m_PositionalDeviceTracker != null)
+        {
+            if (!m_PositionalDeviceTracker.IsActive)
+                m_PositionalDeviceTracker.Start();
+
+            Debug.Log("PositionalDeviceTracker is Active?: " + m_PositionalDeviceTracker.IsActive +
+                      "\nSmartTerrain Tracker is Active?: " + m_SmartTerrain.IsActive);
+        }
+    }
+
+    void OnDevicePoseStatusChanged(TrackableBehaviour.Status status, TrackableBehaviour.StatusInfo statusInfo)
+    {
+        Debug.Log("OnDevicePoseStatusChanged(" + status + ", " + statusInfo + ")");
+    }
+
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIManager : MonoBehaviour { 
+
+    GraphicRaycaster m_GraphicRayCaster;
+    PointerEventData m_PointerEventData;
+    EventSystem m_EventSystem;
+
+    MidAirManager m_midAirManager;
+    TouchHandler m_TouchHandler;
+
+    [SerializeField]
+    private Button resetButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_TouchHandler = FindObjectOfType<TouchHandler>();
+
+        m_GraphicRayCaster = FindObjectOfType<GraphicRaycaster>();
+        m_EventSystem = FindObjectOfType<EventSystem>();
+        m_midAirManager = FindObjectOfType<MidAirManager>();
+
+        Vuforia.DeviceTrackerARController.Instance.RegisterDevicePoseStatusChangedCallback(OnDevicePoseStatusChanged);
+
+        resetButton.onClick.AddListener(()=> {
+            Reset();
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("The UI is being touched: " + IsCanvasPressed());
+    }
+
+    public void Reset()
+    {
+        Debug.Log("Reset was clicked");
+        m_midAirManager.ResetScene();
+
+    }
+
+    public bool IsCanvasPressed()
+    {
+    
+        
+        m_PointerEventData = new PointerEventData(m_EventSystem)
+        {
+            position = Input.mousePosition
+        };
+        List<RaycastResult> results = new List<RaycastResult>();
+        m_GraphicRayCaster.Raycast(m_PointerEventData, results);
+
+        bool resultIsCanvasPressed = false;
+        foreach (RaycastResult result in results)
+        {
+            if (result.gameObject.GetComponentInParent<Toggle>() ||
+                result.gameObject.GetComponent<Button>() ||
+                result.gameObject.GetComponent<CanvasGroup>() ||
+                result.gameObject.GetComponent<Image>())
+            {
+                resultIsCanvasPressed = true;
+                break;
+            }
+        }
+        
+        return resultIsCanvasPressed;
+    }
+
+    void OnDevicePoseStatusChanged(Vuforia.TrackableBehaviour.Status status, Vuforia.TrackableBehaviour.StatusInfo statusInfo)
+    {
+        Debug.Log("OnDevicePoseStatusChanged(" + status + ", " + statusInfo + ")");
+
+        switch (statusInfo)
+        {
+            case Vuforia.TrackableBehaviour.StatusInfo.INITIALIZING:
+                //Debug.Log("Tracker Initializing");
+                break;
+            case Vuforia.TrackableBehaviour.StatusInfo.EXCESSIVE_MOTION:
+                //Debug.Log("Excessive Motion");
+                break;
+            case Vuforia.TrackableBehaviour.StatusInfo.INSUFFICIENT_FEATURES:
+                //Debug.Log("Insufficient Features");
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    void OnDestroy()
+    {
+        Debug.Log("OnDestroy() called.");
+
+        Vuforia.DeviceTrackerARController.Instance.UnregisterDevicePoseStatusChangedCallback(OnDevicePoseStatusChanged);
+    }
+}
